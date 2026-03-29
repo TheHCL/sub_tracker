cb70a9657c330e0aff71d85c9098c88ef58c1dc5
Daniel <daniel8411222@gmail.com>
2026-03-29 14:26:33 +0800

    Initial Commit

---
+++ sub_tracker/sub_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sub_trackerApp.swift
+//  sub_tracker
+//
+//  Created by TheHCL on 2026/3/29.
+//
+
+import SwiftUI
+
+@main
+struct sub_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
